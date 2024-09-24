3ecf34e5331355c27278585c6f7a4b43f759235a
unknown <mehrabmahmoodrishad0744@gmail.com>
2024-09-25 01:39:58 +0600

    final commit!

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let balance = 85000;
 
 document.getElementById('balance').textContent = balance + " BDT";
 
-// Toggle Donation and History Section
+
 document.getElementById('historyBtn').addEventListener('click', function() {
     document.getElementById('donationSection').classList.add('hidden');
     document.getElementById('historySection').classList.remove('hidden');
@@ -15,7 +15,7 @@ document.getElementById('donationBtn').addEventListener('click', function() {
     toggleActive(this, 'historyBtn');
 });
 
-// Common Function to Toggle Active Class
+
 function toggleActive(activeBtn, inactiveBtnId) {
     activeBtn.classList.add('bg-green-700');
     activeBtn.classList.remove('bg-green-500');
@@ -23,7 +23,7 @@ function toggleActive(activeBtn, inactiveBtnId) {
     document.getElementById(inactiveBtnId).classList.add('bg-green-500');
 }
 
-// Donation Function
+
 function donate(cardId, currentDonation) {
     const donationAmountInput = document.getElementById('donationAmount' + cardId);
     const donationAmount = parseInt(donationAmountInput.value);
@@ -45,14 +45,14 @@ function donate(cardId, currentDonation) {
     const newDonationAmount = currentDonation + donationAmount;
     currentDonationElem.textContent = 'Current Donation: ' + newDonationAmount + ' BDT';
 
-    // Add to History
+    
     addToHistory(cardId, donationAmount);
 
-    // Show Success Modal
+  
     document.getElementById('modal').classList.remove('hidden');
 }
 
-// Add Transaction to History
+
 function addToHistory(cardId, amount) {
     const historyList = document.getElementById('historyList');
     const listItem = document.createElement('li');
@@ -63,7 +63,7 @@ function addToHistory(cardId, amount) {
     historyList.appendChild(listItem);
 }
 
-// Close Modal
+
 function closeModal() {
     document.getElementById('modal').classList.add('hidden');
 }
